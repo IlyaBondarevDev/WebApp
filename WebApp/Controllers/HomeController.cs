2fbaad041b3240cfbbc80e959b8f3815ea70e41d
IlyaBondarevDev <james345345345345@gmail.com>
2023-02-04 00:00:36 +1000

    Добавьте файлы проекта.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            var dateTimeInfo = new DateNameModel("Bondarev Ilya Valeryevich");
+            return string.Format("Hello {0}, today is {1:d}",
+                dateTimeInfo.Name, dateTimeInfo.CurrentDate);
+        }
+    }
+}
---
+++ WebApp/Models/DateNameModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Models
+{
+    public class DateNameModel
+    {
+        public string Name { get; }
+        public DateTime CurrentDate { get; set; }
+
+        public DateNameModel(string name)
+        {
+            Name = name;
+            CurrentDate = DateTime.Now;
+        }
+    }
+}
